fe178a79fcce17c222f405ddb5d318b74ac19731
Tubagus M Habiburohman <43885961+M1gadir@users.noreply.github.com>
2019-01-16 14:18:15 +0700

    Add files via upload

---
+++ OOP-1-TB/src/Television/Input.java
@@ -0,0 +1,104 @@
+package Television;
+
+public class Input {
+	//Attribute
+	int volTage;
+	boolean buttonPower;
+	boolean disPlay;
+	int speakerSound;
+	String Brand;
+	boolean HDMI;
+	boolean VGA;
+	boolean indicatorLed;
+	
+	//function voltage 
+	void Voltage() {
+		/*
+	 jadi kalau ingin membuat sebuah method maka bikin dulu keterangannya dan fungsinya
+	 lalu apa itu if ? if adalah sebuah perintah untuk membuat sebuah kondisi contohnya dibawah
+	 jadi if ini mengkondisikan sebuah integer yang apabila variable tersebut di atas 50 maka akan keluar 
+	 output / print "Adapter is compatible" dan berlanjut ke function ynag kita buat yaiut poweron dan kenapa bisa begitu
+	 karena kita memanggil di sebuah method yang sedang berjalan maka akan berlanjut terus ke method selanjutnya
+	 lalu apabila variable tersebut besisi di bawah 50 maka akan keluar print "incompatible is adapter"
+	 jadi fase if itu ada tiga 1. if = awalan kondisi , 2. if else = pertengahan kondisi (banyak kemungkinan)
+	 3. else = kondisi terakhir .  
+		*/
+		if (volTage >= 50) {
+			System.out.println("Adapter Is Compatible");
+			Poweron();
+			}else{
+			System.out.println("Adapter Is Incompatible");
+			
+		}
+	}
+	
+	
+	//function button power
+	void Poweron() {
+		// jadi apabila power button di nyalakan maka akan mengoutput / print television turning on 
+		if (buttonPower) {
+			System.out.println("Television Turning on");
+			// kenapa saya masukan void display karena apabila buttonpower nya di nyalakan maka akan keluar print television is turning on dan lanjut ke display function
+			Display() ;
+		} else{
+			// dan ini kebalikan yang di atas apabila power button false atau di tekan lagi maka akan mengeluarkan / print television turning off
+			System.out.println("Television Turning off");
+		}
+	}
+	String Logo() {
+		return "SAMSNUG";
+	}
+	
+	void Display() {
+		Brand = Logo();
+		/* apabila ingin memasukan boolean ke sebuah statement/ pernyataan.
+		 *  maka harus nulis boolean nya atau true 
+		 *  dan kalau ingin false tinggal kasih tanda seru di depan nama boolean tersebut 
+		 *  contoh nya "!buttonPower"
+		 */
+		if (volTage >= 50 && buttonPower) {
+			disPlay = true ;
+			//jadi ini itu adalah panggilan yang kita buat untuk memanggil inputan lain seperti cek suara, hdmi nya , vga nya, indikator nya
+			// dan kenapa saya masukan banyak function di satu kondisi karena setelah layar nyala kita ngapain 
+			Speakersound();
+			HdMI();
+			VgA();
+			Indikator();
+		}else {
+			disPlay = false ;
+		}
+	}
+	// function speaker
+	void Speakersound() {
+		// jadi kita membuat konndisi speaker apabila di bawah 5 maka akan keluar output I Can't Hear Sound"
+		if (speakerSound <= 5) {
+			System.out.println("Speaker Is Working");
+		}else {
+			System.out.println("Speaker Is Working");
+		}
+	}
+	//function hdmi
+	void HdMI() {
+		if (HDMI) {
+		System.out.println("HDMI is conected");
+		}else {
+		System.out.println("HDMI Not Found");
+		}
+	}
+	// function vga
+	void VgA() {
+		if (VGA) {
+			System.out.println("VGA is conected");
+		}else {
+		System.out.println("VGA Not Found");
+		}
+	}
+	// function vga
+	void Indikator() {
+		if (indicatorLed) {
+			System.out.println("indikator sudah menyala");
+		}else {
+			System.out.println("indikator belum menyala");
+		}
+	}
+}
---
+++ OOP-1-TB/src/Television/Output.java
@@ -0,0 +1,23 @@
+package Television;
+
+public class Output {
+public static void main (String [] args) {
+	Input input = new Input() ;
+	input.volTage = 60 ;
+	input.buttonPower = true ;
+	input.disPlay = true ;
+	input.speakerSound = 10 ;
+	input.HDMI = true ;
+	input.VGA = true ;
+	input.indicatorLed = true ;
+	input.Brand = "" ;
+	
+	/* jadi di sini itu sebagai
+	 *  memproses sebuah function yang kita awal buat jadi 
+	 *  input.voltage(); itu sebuah method yang memiliki sebuah funsi atau ada fungsinya
+	 *  
+	 *  
+	 */
+	input.Voltage();
+}
+}
